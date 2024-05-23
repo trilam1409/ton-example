a2f3a59c3fce74d22404f47f2b4c1bacdac8d297
Tri Lam <lamviettri1996@gmail.com>
2024-05-23 15:09:35 +0700

    Commit

--- app/Button.tsx
+++ app/Button.tsx
@@ -1,17 +1,29 @@
 import {useTonConnectUI} from "@tonconnect/ui-react";
+import {useEffect, useState} from "react";
 
 const Button = () => {
   const [tonConnectUI, setOptions] = useTonConnectUI();
+
+  const [wallet, setWallet] = useState([])
+
+  // useEffect(() => {
+  //   (async () => {
+  //     const walletList = tonConnectUI.getWallets();
+  //
+  //   })()
+  // }, []);
 
   const transaction: any = {
     messages: [
       {
         address:
-          "0QAHHsAap0OqtLf-v385nfRJ0rU-8-vQtm3BefwfZDUWY_Tj", // destination address
-        amount: "10000000", //Toncoin in nanotons
+          "0QAHHsAap0OqtLf-v385nfRJ0rU-8-vQtm3BefwfZDUWY_Tj",
+        amount: "20000000",
+        validUntil: Math.floor(Date.now() / 1000) + 60
       },
     ],
   };
+
 
   return (
     <button onClick={async () => {
